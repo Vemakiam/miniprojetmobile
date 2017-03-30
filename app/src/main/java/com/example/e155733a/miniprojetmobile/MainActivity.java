91a819f69832099968b25c36b7fb6b31e6212570
Vemakiam <damien.voreiter@gmail.com>
2017-03-30 14:15:53 +0200

    Ajout de la librairie volley pour les requêtes à'API

--- app/src/main/java/com/example/e155733a/miniprojetmobile/MainActivity.java
+++ app/src/main/java/com/example/e155733a/miniprojetmobile/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.e155733a.miniprojetmobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest =
+                new StringRequest(
+                        Request.Method.POST,
+                        "http://www.perdu.com",
+                        new Response.Listener<String>() {
+                            public void onResponse(String response) {
+                                TextView visu = (TextView) findViewById(R.id.bonjour);
+                                visu.setText(response);
+                            }},
+                        new Response.ErrorListener() {
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("VOLLEY", error.getMessage());
+                            }})
+                {
+                    protected Map<String,String> getParams(){
+                        Map<String,String> params = new HashMap<String, String>();
+                        params.put("nom", "Lanoix");
+                        return params;
+                    }
+                };
+        queue.add(stringRequest);
     }
 }
